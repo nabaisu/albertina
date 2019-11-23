8f804c2cad445d96aabba9b457875900021ed793
nabaisu <nabais@nabaisu.com>
2019-11-23 16:51:36 +0100

    made first test test, pass

---
+++ app/app/calc.py
@@ -0,0 +1,3 @@
+def add(x,y):
+    """add 2 numbers"""
+    return x + y
---
+++ app/app/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from app.calc import add
+
+class CalcTests(TestCase):
+    def test_add_numbers(self):
+        #test that 2 numbers are added together
+        self.assertEqual(add(3,8), 11)
